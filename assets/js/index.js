370fe025cac340afc8aef312f4ac1d7e8bfaff0a
JulioRehbein <julio.rehbein@edu.udla.cl>
2024-05-17 16:01:08 -0400

    0.5

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+var navbar = document.getElementById('navbar'); 
+
+navbar.addEventListener('mouseenter', function() {
+    navbar.style.display = 'block';
+});
+
+navbar.addEventListener('mouseleave', function() {
+    navbar.style.display = 'none';
+});
